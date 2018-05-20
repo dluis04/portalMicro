146a5bd363bc3a00d5543b7504e9b6e7944a08d3
Diego Luis <dluis_04@hotmail.com>
2018-05-19 19:37:17 -0500

    saludo 2

--- portalMycrosystems/ViewPortalMycrosystems/src/com/psicovirtual/liquidadorAdminTotal/vista/mb/MBLogin.java
+++ portalMycrosystems/ViewPortalMycrosystems/src/com/psicovirtual/liquidadorAdminTotal/vista/mb/MBLogin.java
@@ -40,6 +40,7 @@ public class MBLogin implements Serializable{
 			if (dnUsuarios.consultarUsuarioInicio(usuario) == 1) {
 
 				System.out.println("Hola Diego");
+				System.out.println("Hola Jhon");
 				Usuario logueado= dnUsuarios.consultarDetalleUsuario(usuario.getCedula());
 				Date fecha = new Date();
 				sesion.setUsuario(logueado);
